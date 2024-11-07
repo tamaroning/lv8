14e5a06532633e48aa6ca162b2fdf7ab535cd0c6
Raiki Tamura <tamaron1203@gmail.com>
2024-11-07 15:18:54 +0900

    formatting

--- src/driver.rs
+++ src/driver.rs
@@ -5,7 +5,11 @@ use std::path::PathBuf;
 use crate::runtime::{self};
 
 #[derive(Parser)]
-#[clap(name = "lv8", version = env!("CARGO_PKG_VERSION"), about = "lv8 is a WebAssembly runtime")]
+#[clap(
+    name = "lv8",
+    version = env!("CARGO_PKG_VERSION"),
+    about = "lv8 is a WebAssembly runtime"
+)]
 pub struct Cli {
     pub wasmfile_path: PathBuf,
 }
